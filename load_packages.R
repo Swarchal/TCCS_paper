5b3aa53e4d8fa383e98d998abcd3f984bf605322
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-07-05 17:54:29 +0100

    needs gtools

--- load_packages.R
+++ load_packages.R
@@ -3,7 +3,7 @@
 load_packages <- function(){
     # checks if packages are already installed,
     # installs packages if needed
-    
+
     cran_packages <- c(
         "devtools",
         "dplyr",
@@ -11,24 +11,25 @@ load_packages <- function(){
         "reshape2",
         "readr",
         "caret",
+        "gtools",
         "viridis",
         "gplots",
         "ggplot2"
     )
-    
+
     github_packages <- c(
         "phenoScreen",
         "phenoDist",
         "focus",
         "Smisc"
     )
-    
+
     for (package in cran_packages){
         if (!require(package, character.only = TRUE)){
             install.packages(package)
         }
     }
-    
+
     for (g_package in github_packages){
         if (!require(g_package, character.only = TRUE)){
             devtools::install_github(paste("swarchal", g_package, sep = "/"))
