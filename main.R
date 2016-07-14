94294fe6ce7a91cf65e5f5972776404d074899d0
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-07-14 10:50:08 +0100

    use OS file sep

--- main.R
+++ main.R
@@ -2,6 +2,6 @@ files <- list.files("analysis")
 source("load_packages.R")
 for (file in files){
     cat(paste("- Running", file, "\n"))
-    source(paste("analysis", file, sep = "/"))
+    source(paste("analysis", file, sep = .Platform$file.sep))
 }
 cat(paste(" - DONE", "\n"))
